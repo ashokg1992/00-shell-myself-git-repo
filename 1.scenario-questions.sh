10c95393df0d6bd429db568a0241664218e9af3f
ashok <ashokreddyothers@gmail.com>
2024-05-26 15:11:22 +0530

    add

--- 1.scenario-questions.sh
+++ 1.scenario-questions.sh
@@ -1,10 +1,26 @@
+
+---------------------------------------------
 Script File Name Adaptability:
 
 Scenario 1 discusses how to write a shell script that adapts to changes in its file name, ensuring the output reflects the new name without hardcoding the file name inside the script. This is achieved using $0 to dynamically capture the script's name.
-Using Configuration Files:
+   file1.sh
+     echo "this is $0"
+    
+    now we run this file 
+    sh file1.sh   # o/p: this is file one
+    now in same file run as 
+    mv file1.sh file2.sh
+    sh file2.sh     # o/p ia thia is file2.sh
 
+ 
+ --------------------------------------------------------------
+Using Configuration Files:
 Scenario 2 highlights the importance of avoiding hardcoding values by loading configuration properties from an external config file using the source command. This approach makes it easier to manage and update properties used across multiple scripts.
 Handling Script Options with Case and Shift:
+
+i have applicatiatin properties file and i use it in my shell script, then how you do?
+    - we maintain two files - application , shell files,. in shell file we give source as application file and  call them it this file
+----------------------------------------------------------------------------
 
 Scenario 3 focuses on handling multiple script options (-B, -T, etc.) using a combination of the case statement and the shift command. This method simplifies the script logic and makes it more maintainable, especially when adding new options.
 
