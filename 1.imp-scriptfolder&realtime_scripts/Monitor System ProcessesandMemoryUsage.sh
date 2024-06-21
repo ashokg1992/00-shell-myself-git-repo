128ccdcce1386d2cf7f417cb46a021e7863efc2e
ashok <ashokreddyothers@gmail.com>
2024-06-21 19:14:38 +0530

    add

---
+++ 1.imp-scriptfolder&realtime_scripts/Monitor System ProcessesandMemoryUsage.sh
@@ -0,0 +1,15 @@
+
+10. Monitor System Processes and Memory Usage
+Script
+Script
+#!/bin/bash
+# Monitor system processes and their memory usage
+ps aux --sort=-%mem | head -n 10
+# Explanation
+# • ps aux: Lists all running processes.
+
+# • --sort=-%mem: Sorts the processes by memory usage in descending order.
+
+# • head -n 10: Displays the top 10 processes by memory usage.
+
+ 
